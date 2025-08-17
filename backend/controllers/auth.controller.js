6fc061291dcc379c9d2b5bfc7de8167679a7c7d9
mystic1153 <yug35287@gmail.com>
2025-08-17 16:31:43 +0530

    cookie fixed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -23,13 +23,13 @@ const setCookies = (res, accessToken, refreshToken) => {
     res.cookie("accessToken", accessToken, {
         httpOnly: true, //prevents XSS attacks
         secure: process.env.NODE_ENV === "production",
-        sameSite:"strict", //prevents CSRF attacks, cross-site request forgery attack
+        sameSite:"None", //prevents CSRF attacks, cross-site request forgery attack
         maxAge: 15 * 60 * 1000, // 15 minutes
     })
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true, 
         secure: process.env.NODE_ENV === "production",
-        sameSite:"strict",
+        sameSite:"None",
         maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
     })
 };
@@ -125,7 +125,7 @@ export const refreshToken = async (req, res) => {
         res.cookie("accessToken", accessToken, {
             httpOnly: true, 
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "None",
             maxAge: 15 * 60 * 1000,
         });
 
