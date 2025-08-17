73523d98b44ebb2ea94c29431a49182b18663e79
mystic1153 <yug35287@gmail.com>
2025-08-17 15:57:50 +0530

    vite url imported

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-
+import dotenv from "dotenv";
+dotenv.config();
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
-
 export default axiosInstance;
