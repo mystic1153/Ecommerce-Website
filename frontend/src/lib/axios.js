d38bc25dee0ab800f34474e8da3d072ca9714d86
Yug Patel <165924309+mystic1153@users.noreply.github.com>
2025-08-17 15:33:53 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "https://ecommerce-website-lq3d.onrender.com",
 	withCredentials: true, // send cookies to the server
 });
 
